06aa66e059df3e0a9c7419a024031103f2823703
“sunshine4Cpic <sunxujianxiou@sina.com>
2016-10-20 14:51:31 +0800

    1.1更新

--- publish/wwwroot/js/site.js
+++ publish/wwwroot/js/site.js
@@ -17,5 +17,19 @@ $(function () {
                 jqObj.find("[name='" + i + "']").val(data[i]);
 
         }
-    }
+    };
+
+    jQuery.download = function (url, data, method) {    // 获得url和data
+        if (url && data) {
+            // data 是 string 或者 array/object
+            data = typeof data == 'string' ? data : unescape(jQuery.param(data));    // 把参数组装成 form的  input
+            var inputs = '';
+            jQuery.each(data.split('&'), function () {
+                var pair = this.split('=');
+                inputs += '<input type="hidden" name="' + pair[0] + '" value="' + pair[1] + '" />';
+            });        // request发送请求
+            jQuery('<form action="' + url + '" method="' + (method || 'post') + '">' + inputs + '</form>')
+            .appendTo('body').submit().remove();
+        };
+    };
 });
